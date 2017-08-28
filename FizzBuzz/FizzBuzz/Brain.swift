863765ebdc4225e20a74e3384dadeb2c3123d99c
Carlos Cortés Sánchez <ccortes@gmv.com>
2017-08-28 15:05:37 +0200

    Refactor code to test success

--- FizzBuzz/FizzBuzz/Brain.swift
+++ FizzBuzz/FizzBuzz/Brain.swift
@@ -11,6 +11,10 @@ import Foundation
 class Brain {
     
     func isDivisibleByThree (number: Int) -> Bool {
-        return true
+        if number % 3 == 0 {
+            return true
+        } else {
+            return false
+        }
     }
 }
