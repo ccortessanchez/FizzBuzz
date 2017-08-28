8d59c32b91a44661adc8533c914a34c999f07166
Carlos Cortés Sánchez <ccortes@gmv.com>
2017-08-28 16:00:30 +0200

    Add Game tests

---
+++ FizzBuzz/FizzBuzz/Game.swift
@@ -0,0 +1,13 @@
+//
+//  Game.swift
+//  FizzBuzz
+//
+//  Created by Carlos Cortés Sánchez on 28/08/2017.
+//  Copyright © 2017 Carlos Cortés Sánchez. All rights reserved.
+//
+
+import Foundation
+
+class Game {
+    
+}
---
+++ FizzBuzz/FizzBuzzTests/GameTests.swift
@@ -0,0 +1,24 @@
+//
+//  GameTests.swift
+//  FizzBuzz
+//
+//  Created by Carlos Cortés Sánchez on 28/08/2017.
+//  Copyright © 2017 Carlos Cortés Sánchez. All rights reserved.
+//
+
+import XCTest
+
+class GameTests: XCTestCase {
+    
+    let game = Game()
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+}
