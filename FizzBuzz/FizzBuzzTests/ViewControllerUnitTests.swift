180026906b34bc513a312f44320c526bbc1309dc
Carlos Cortés Sánchez <ccortes@gmv.com>
2017-08-29 09:49:43 +0200

    Set class for ViewController test

---
+++ FizzBuzz/FizzBuzzTests/ViewControllerUnitTests.swift
@@ -0,0 +1,30 @@
+//
+//  ViewControllerUnitTests.swift
+//  FizzBuzz
+//
+//  Created by Carlos Cortés Sánchez on 29/08/2017.
+//  Copyright © 2017 Carlos Cortés Sánchez. All rights reserved.
+//
+
+import XCTest
+@testable import FizzBuzz
+
+class ViewControllerUnitTests: XCTestCase {
+    
+    var viewController: ViewController!
+    
+    override func setUp() {
+        super.setUp()
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        viewController = storyboard.instantiateViewController(withIdentifier: "ViewController") as! ViewController
+        UIApplication.shared.keyWindow!.rootViewController = viewController
+        
+        let _ = viewController.view
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+}
